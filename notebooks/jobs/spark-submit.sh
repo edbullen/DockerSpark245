80d4a5d80b17b6996c7ecd102dda7f78c9ae20d6
Ed Bullen <bullee@amazon.co.uk>
2022-03-15 12:20:53 +0000

    add Kafka Java libraries to spark-submit

--- notebooks/jobs/spark-submit.sh
+++ notebooks/jobs/spark-submit.sh
@@ -17,5 +17,6 @@ fi
 
 spark-submit --master spark://spark-master:7077 --num-executors 2 \
 	     --executor-memory $EXEC_MEM --executor-cores 1 \
+             --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 \
              $PYTHON_JOB
 
